04ccb14496dd3d47ad7751e21bca6693613846da
josue omar flores diaz <josue.omar68u@std.uni.edu.ni>
2022-03-31 19:08:06 -0600

    Funcion añadir Empleado

--- Infraestructure/Repository/EmpleadoRepository.cs
+++ Infraestructure/Repository/EmpleadoRepository.cs
@@ -8,7 +8,77 @@ using System.Threading.Tasks;
 
 namespace Infraestructure.Repository
 {
-    public class EmpleadoRepository : BaseRepositoy<Empleado>, IEmpleadoModel
+    public class EmpleadoRepository : IEmpleadoModel
     {
+        public RAFContext context;
+        public const int SIZE = 119;
+        public EmpleadoRepository()
+        {
+            context = new RAFContext("empleado", SIZE);
+        }
+        public void Add(Empleado t)
+        {
+            try
+            {
+                context.Create<Empleado>(t);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public bool Delete(Empleado t)
+        {
+
+            try
+            {
+                return context.Delete<Empleado>(t);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public Empleado GetById(int id)
+        {
+
+            try
+            {
+                return context.Get<Empleado>(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<Empleado> Read()
+        {
+
+            try
+            {
+                return context.GetAll<Empleado>();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public int Update(Empleado t)
+        {
+            try
+            {
+                return context.Update<Empleado>(t);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+       
     }
 }
